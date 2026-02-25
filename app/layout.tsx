dfab6dbb91f14253b33eb6e6b270e2e3e3c6b176
Xavier Liogier <xlddev@MacBook-Pro.lan>
2026-02-25 15:43:04 +0100

    Analytics added

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import Link from "next/link";
 import "./globals.css";
+import { Analytics } from '@vercel/analytics/next';
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -42,6 +43,7 @@ export default function RootLayout({
         >
           Study update <span className="text-white/80">↗</span>
         </Link>
+        <Analytics />
       </body>
     </html>
   );
